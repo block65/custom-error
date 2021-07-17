f159d27a279cc437fca93d8e565cfee269963de6
Max Holman <max@holmn.com>
2021-07-17 23:07:16 +0800

    chore: export interfaces + remove redundant enum

--- lib/index.ts
+++ lib/index.ts
@@ -41,25 +41,25 @@ const defaultHttpMapping: Map<Status, number> = new Map([
   [Status.DEADLINE_EXCEEDED, 504],
 ]);
 
-interface ErrorInfo {
+export interface ErrorInfo {
   reason: string;
   metadata: Record<string, string>;
 }
 
-interface RetryInfo {
+export interface RetryInfo {
   delay: number;
 }
 
-interface BadRequest {
+export interface BadRequest {
   violations: { field: string; description: string }[];
 }
 
-interface LocalisedMessage {
+export interface LocalisedMessage {
   locale: 'en';
   message: string;
 }
 
-interface Help {
+export interface Help {
   url: string;
   description: string;
 }
@@ -75,29 +75,6 @@ export interface QuotaFailure {
      */
     description: string;
   }[];
-}
-
-export enum ErrorReason {
-  ERROR_REASON_UNSPECIFIED = 0,
-  SERVICE_DISABLED = 1,
-  BILLING_DISABLED = 2,
-  API_KEY_INVALID = 3,
-  API_KEY_SERVICE_BLOCKED = 4,
-  API_KEY_HTTP_REFERRER_BLOCKED = 7,
-  API_KEY_IP_ADDRESS_BLOCKED = 8,
-  API_KEY_ANDROID_APP_BLOCKED = 9,
-  API_KEY_IOS_APP_BLOCKED = 13,
-  RATE_LIMIT_EXCEEDED = 5,
-  RESOURCE_QUOTA_EXCEEDED = 6,
-  LOCATION_TAX_POLICY_VIOLATED = 10,
-  USER_PROJECT_DENIED = 11,
-  CONSUMER_SUSPENDED = 12,
-  CONSUMER_INVALID = 14,
-  SECURITY_POLICY_VIOLATED = 15,
-  ACCESS_TOKEN_EXPIRED = 16,
-  ACCESS_TOKEN_SCOPE_INSUFFICIENT = 17,
-  ACCOUNT_STATE_INVALID = 18,
-  ACCESS_TOKEN_TYPE_UNSUPPORTED = 19,
 }
 
 type ErrorDetail =
