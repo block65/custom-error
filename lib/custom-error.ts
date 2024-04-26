de3db28d2eae2ad9074cf9e4fd9b48ade633c56b
Max Holman <max@holmn.com>
2024-04-26 15:15:30 +0800

    feat: better error hydration using status/code as message if needed

--- lib/custom-error.ts
+++ lib/custom-error.ts
@@ -244,14 +244,17 @@ export class CustomError extends Error {
    * @return {CustomError}
    */
   public static fromJSON(params: CustomErrorSerialized): CustomError {
-    return Object.assign(
-      new CustomError(params.message || 'Error').addDetail(
-        ...(params.details || []),
-      ),
-      {
-        code: params.code || Status.UNKNOWN,
-      },
-    );
+
+    const {code = Status.UNKNOWN, message, details = [], ...rest} = params;
+
+    const err = new CustomError(message || (Status[params.code] || params.code || 'Error').toString()).debug({params})
+    err.code = code;
+
+    if (details) {
+      err.addDetail(...details)
+    }
+
+    return err;
   }
 
   /**
